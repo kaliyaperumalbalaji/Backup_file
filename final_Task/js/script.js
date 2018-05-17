7d7d01dd3909560a5009cf307a638a6b7f9d037d
kaliyaperumalbalaji <32338237+kaliyaperumalbalaji@users.noreply.github.com>
2018-05-17 16:53:29 +0530

    Add files via upload

---
+++ final_Task/js/script.js
@@ -0,0 +1,17 @@
+$('.myimages').click(function(){
+
+   	var imgsrc = $(this).attr('src');
+
+	$('div').removeClass('border-color');
+	var getclass = $(this).closest('div').attr('class');
+	$('.'+getclass).addClass('border-color');
+
+	$('.box-img').css({"background-image":"url('"+imgsrc+"')","background-size":"100% 100%","background-repeat":"no-repeat"});
+
+	
+var a = $(this).parent().attr('class');
+	$(a).css('border','10px solid pink');
+
+
+});
+
